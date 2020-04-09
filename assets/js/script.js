2137bc15f58584404ed48d9befe72bbcbe59e4f8
lovelikehee <yeonhee_dev@naver.com>
2020-04-09 23:28:42 +0900

    script 수정

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,18 @@
 $(function(e){
     e.preventDefalut;
- let dot =  $(".carousel").find("li");
+    let dot = $(".carousel").find("li"),
+          headerH =   $(".main-gnb").outerHeight();
 
-dot.click(function(e){
-    e.preventDefalut;
-    dot.removeClass("active")
-    $(this).toggleClass("active");
-    $(".dot").find("a").addClass("opacity");
-    $(".dot").find("p, .column-line, .row-line").addClass("transform");
-
-});
-
-
-
-
-// let dot = $(".carousel").find("li");
-
-// dot.click(function(e){
-//     e.preventDefault();
-//     var target = $(this);
-//     var index = target.index();
-//     //alert(index);
-
- 
-// });
+    dot.click(function(){
+        dot.removeClass("active")
+        $(this).toggleClass("active");
+        $(".dot").find("a").addClass("opacity");
+        $(".dot").find("p, .column-line, .row-line").addClass("transform");
+    });
+          
+      
+    $(".section-hero .sticky-wrapper").css("top", -headerH);
+    console.log(headerH);
 
 
 
