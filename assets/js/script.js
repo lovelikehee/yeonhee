d4ad1c8a68e77f37f60231c22504dc9091838d1a
lovelikehee <yeonhee_dev@naver.com>
2020-04-13 11:51:14 +0900

    media 376

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,33 +8,28 @@ let height = window.innerHeight;
 let carousel = $(".carousel-wrap li"); 
 let circle = $(".circle-items");
 
-
 //scrollTop value
     $(window).scroll(function(){
         let Scroll = $(window).scrollTop();          
         $(".Scroll").text(Scroll);
-
-
-  
-
+            //.triptych-column Scroll
             let offset = (Scroll - $(".triptych-column").offset().top )* 0.18;
-            let offset1 = (Scroll - $(".column-right").offset().top )* 0.07;
-  
+            let offset1 = (Scroll - $(".column-right").offset().top )* 0.07; 
             $(".triptych-column .triptych-image-container  figure").css({"transform":"translateY("+ -offset+"px)"});
             $(".gallery-img-all").css({"transform":"translateY("+ -offset+"px)"});
             $(".work-typography").css({"transform":"translateY("+ -offset1+"px)"});
 
-
-
     });
-
 
     //webfont load가 script load보다 늦어서 setTimeout 300을 줌.
     setTimeout(function(){
-        var  headerH = $(".main-gnb").outerHeight();
+        let  headerH = $(".main-gnb").outerHeight();
         $(".section-hero .sticky-wrapper").css("top", -headerH);
         console.log(headerH);
     },300);
+
+    let svgW = $(".svgText").outerWidth();
+    console.log(svgW);
 
     //carousel click motion
     carousel.click(function(){
