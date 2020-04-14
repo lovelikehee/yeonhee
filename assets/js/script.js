ed20ab3681bacd5f8b48b9f640763c4ce093893c
lovelikehee <yeonhee_dev@naver.com>
2020-04-14 20:08:56 +0900

    commit3

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,31 @@ $(function (e) {
     let width = window.innerWidth;
     let height = window.innerHeight;
     let carousel = $(".carousel-wrap li");
-    let circle = $(".circle-items");
+    let circle = $(".rect-items");
     let contItems = $(".contact-items").find("input");
 
     //scrollTop value
     $(window).scroll(function () {
         let Scroll = $(window).scrollTop();
-        $(".Scroll").text(Scroll);
-        //.triptych-column Scroll
+        let ScrollH = Math.ceil(Scroll);
+        $(".Scroll").text(ScrollH);
+
+
+
         let offset = (Scroll - $(".triptych-column").offset().top) * 0.18;
         let offset1 = (Scroll - $(".column-right").offset().top) * 0.07;
-        $(".triptych-column .triptych-image-container  figure").css({
-            "transform": "translateY(" + -offset + "px)"
+        let offset2 = (Scroll - $(".triptych-column").offset().top) /1000;
+        let offset3 = (Scroll - $(".section-about").offset().top) *0.18;
+        //.ability
+        $(".about-display").css({
+            "transform": "scale("+ -offset2 +") translateY(" + offset3+"px)"
+       
         });
-        $(".gallery-img-all").css({
-            "transform": "translateY(" + -offset + "px)"
-        });
-        $(".work-typography").css({
-            "transform": "translateY(" + -offset1 + "px)"
-        });
+
+        //.triptych-column Scroll
+        $(".triptych-column .triptych-image-container  figure").css({ "transform": "translateY(" + -offset + "px)"});
+        $(".gallery-img-all").css({"transform": "translateY(" + -(offset1-60) + "px)" });
+        $(".work-typography").css({ "transform": "translateY(" + -(offset1-50) + "px)"});
 
     });
 
@@ -42,7 +48,14 @@ $(function (e) {
         $(".carousel").find("a").addClass("opacity");
         $(".carousel").find("p, .column-line, .row-line").addClass("transform");
     });
+});
 
+$(function(){
+
+
+
+
+});
 
     // //ability slide
     //          let index = 1;
@@ -58,18 +71,11 @@ $(function (e) {
     //          },1000);
 
 
-
-
     //.contacts label 
-
-
-
-
-
     $(function () {
         $("#uName").keydown(function (key) {
-            key.preventDefault();
             if (key.keyCode == 13) { // name 입력부분에서 엔터키를 쳤을 경우
+                key.preventDefault();
                 $("#uMail").focus(); // mail 입력부분으로 포커스 이동한다
             } 
         });
@@ -80,18 +86,19 @@ $(function (e) {
         });
         $("#uMemo").keydown(function (e) {
             if (e.keyCode == 13) {
-                $(".contact-button").focus(); // pw 입력부분에서 엔터를 치면 submit 실행 
+                $(".contact-button").focus(); // button 입력부분으로 포커스 이동한다
             }
         });
-
         $(".input").focus(function () {
             $(this).siblings(".line").css('background', '#02F093');
-            $(this).parent(".contact-input").siblings(".label-text").addClass("active");
         });
         $(".input").blur(function () {
-            // 입력커서를 벗어났을시 초기화 
             $(this).siblings(".line").css('background', '#fff');
         });
-
+        $(".input").change(function () {
+            $(this).parent(".contact-input").siblings(".label-text").addClass("active");
+        });
     });
-});
+
+
+
