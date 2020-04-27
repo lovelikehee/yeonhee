ffe2e12783724ffa9fa3185b962b70c3a6be09b4
lovelikehee <yeonhee_dev@naver.com>
2020-04-27 20:52:22 +0900

    gnb 수정

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,81 +7,81 @@ $(function(e) {
     let contItems = $(".contact-items").find("input");
     let section = $(".main").find("section");
     let menu = $(".navlist").find("li");
-    let Scroll = $(window).scrollTop();
-    let ScrollH = Math.ceil(Scroll);
     let contH = $(".section").offset().top;
     //Height 값구하기 
     let headerH = $(".main-gnb").outerHeight();
     let pagingH = $(".paging").outerHeight();
     let currentH = $(".current").find("li").outerHeight();
 
-
     //scrollTop value
-    $(window).scroll(function (e) {
-        e.preventDefalut;
+    $(window).scroll(function() {
+        let Scroll = $(window).scrollTop();
+        let ScrollH = Math.ceil(Scroll);
         $(".Scroll").text(ScrollH);
 
         //offset 
-        let offset = (Scroll / contH) * 2;
-        let offset1 = (Scroll - $(".column-right").offset().top) * 0.07;
-        let offset2 = (Scroll - $(".triptych-column").offset().top) / 5;
-        let offset3 = (Scroll - $(".section-about").offset().top) * 0.18;
-        let offset4 = (Scroll - $(".ability-content").offset().top) * 0.05;
-        let offset5 = (Scroll - $(".hero-manifesto").offset().top) * 0.23;
-        let offset6 = (Scroll - $(".section-contact").offset().top) * 0.2;
+        let offset = (Scroll / contH)*2;
+        let offset1 = (Scroll - $(".column-right").offset().top)*0.07;
+        let offset2 = (Scroll - $(".triptych-column").offset().top)/5;
+        let offset3 = (Scroll - $(".section-about").offset().top)*0.18;
+        let offset4 = (Scroll - $(".ability-content").offset().top)*0.05;
+        let offset5 = (Scroll - $(".hero-manifesto").offset().top)*0.23;
+        let offset6 = (Scroll - $(".section-contact").offset().top)*0.2;
 
         //.hero
-        $(".typography-hero-subhead").css({"opacity": offset / 10.5,"transform": "matrix(1, 0, 0, 1, 0," + offset + ")"});
-        $(".hero-copy").css({"opacity": offset / 11,"transform": "matrix(1, 0, 0, 1, 0," + offset + ")"});
+        $(".typography-hero-subhead").css({"opacity": offset/10.5,"transform": "matrix(1, 0, 0, 1, 0," + offset + ")"});
+        $(".hero-copy").css({"opacity": offset/11,"transform": "matrix(1, 0, 0, 1, 0," +offset+ ")"});
         //.about
-        $(".about-display").css({"transform": "matrix(" + 70 / offset + ", 0, 0, " + 70 / offset + " ,0," + -offset4 * 1.6 + ")"});
-        $(".screm").css({"opacity": -offset4 / 480});
-        $(".fill-screen-bg").css({ "transform": "matrix( 1, 0, 0, 1 ,0," + -offset5 * 1.1 + ")"});
+        $(".about-display").css({"transform": "matrix(" +70/offset+ ", 0, 0, " +70/offset+ " ,0," +-offset4*1.6 + ")"});
+        $(".screm").css({"opacity": -offset4/480});
+        $(".fill-screen-bg").css({ "transform": "matrix( 1, 0, 0, 1 ,0,"+-offset5*1.1 +")"});
         //.triptych-column Scroll
-        $(".triptych-column .triptych-image-container  figure").css({ "transform": "translateY(" + offset2 + "px)"});
-        $(".gallery-img-all").css({ "transform": "translateY(" + -(offset1 - 60) + "px)" });
-        $(".work-typography").css({"transform": "translateY(" + -(offset1 - 50) + "px)"});
+        $(".triptych-column .triptych-image-container  figure").css({ "transform": "translateY("+offset2+"px)"});
+        $(".gallery-img-all").css({ "transform": "translateY("+ -(offset1-60) + "px)" });
+        $(".work-typography").css({"transform": "translateY(" + -(offset1-50) + "px)"});
 
         $(".contact-wrap").css({"transform": "translateY(" + -offset6*1.4+ "px)"});
 
 
         if (Scroll >= section.eq(0).offset().top) {
             $(".carousel-wrap > li").eq(0).addClass("active");
+            menu.removeClass("show");
         } else {
             $(".carousel-wrap > li").eq(0).removeClass("active");
         }
         if (Scroll >= section.eq(1).offset().top) {
             $(".section-about").addClass("active");
             $(".carousel-wrap> li").eq(1).addClass("active");
-            menu.eq(1).addClass("active");
+            menu.eq(1).addClass("show");
         } else {
             $(".section-about").removeClass("active");
             $(".carousel-wrap> li").eq(1).removeClass("active");
+            menu.eq(1).removeClass("show");
         }
-        if (Scroll >= section.eq(2).offset().top / 1.2) {
+        if (Scroll >= section.eq(2).offset().top/1.2) {
             $(".carousel-wrap >li").eq(2).addClass("active");
-            menu.eq(1).removeClass("active");
-            menu.eq(0).addClass("active");
+            menu.eq(1).removeClass("show");
         } else {
             $(".carousel-wrap >li").eq(2).removeClass("active");
         }
         if (Scroll >= section.eq(3).offset().top) {
             $(".carousel-wrap >li").eq(3).addClass("active");
+            menu.eq(0).addClass("show");
         } else {
             $(".carousel-wrap >li").eq(3).removeClass("active");
         }
         if (Scroll >= section.eq(4).offset().top) {
             $(".carousel-wrap >li").eq(4).addClass("active");
+            menu.eq(0).removeClass("show");
         } else {
             $(".carousel-wrap >li").eq(4).removeClass("active");
         }
         if (Scroll >= section.eq(5).offset().top) {
             $(".carousel-wrap >li").eq(5).addClass("active");
-            menu.eq(0).removeClass("active");
-            menu.eq(2).addClass("active");
+            menu.eq(2).addClass("show");
         } else {
             $(".carousel-wrap >li").eq(5).removeClass("active");
-            menu.eq(2).removeClass("active");
+            menu.eq(2).removeClass("show");
         }
     });
 
@@ -92,7 +92,6 @@ $(function(e) {
         $(".section-hero").addClass("active");
         console.log(currentH);
     }, 300);
-
 
     //nav
     menu.find("a").click(function () {
@@ -105,7 +104,6 @@ $(function(e) {
     let current = 0;
     let item = $(".ability-content").find("li");
     let items = item.length;
-    console.log(items);
 
     $(".next").click(function () {
         item.eq(idx).css({"margin-left": "110%" }).animate({"margin-left": 0}, 300);
